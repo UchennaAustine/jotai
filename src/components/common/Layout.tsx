cc012f3e7dfecef65fca63928985fd3aa8bcc551
UchennaAustine <uchennaaustine8@gmail.com>
2023-10-10 08:52:31 +0100

    add

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
-import Header from "../static/Header";
-import Footer from "../static/Footer";
+// import Header from "../static/Header";
+// import Footer from "../static/Footer";
 
 const Layout = () => {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
